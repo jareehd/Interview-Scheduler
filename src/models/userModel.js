84971f2b38e3a501e5092d64da0160be74ca2b77
jareehd <dheeraj945016@gmail.com>
2021-06-23 23:11:15 +0530

    added express server

---
+++ src/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const validator = require('validator') 
+
+const userSchema = new mongoose.Schema({
+    firstname : {
+        type : String,
+        required:true
+    },
+    lastname:{
+        type : String
+    },
+    email: {
+        type: String,
+        required:true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    } 
+})
+ 
+const User =  mongoose.model('User',userSchema)
+
+module.exports = User
