75906feb0b4b2daaf5cbe23bde28de24624d90b1
jareehd <dheeraj945016@gmail.com>
2021-06-25 00:01:16 +0530

    set up production

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const mongoose = require('mongoose') 
 const usersRouter = require('./routes/userRoutes')
 const interviewRouter = require('./routes/interviewRoutes')
-
+const path = require('path')
 app.use(cors())
 app.use(express.json({limit : '20mb'}))
 app.use(express.urlencoded({ extended: true , limit: '20mb' }))
@@ -23,6 +23,12 @@ mongoose.connect( db ,{
 })
 
 const port = process.env.PORT || 5000
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port,()=>{
     console.log('Server is running on port, ' ,port)
