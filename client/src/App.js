cc7eeb6a05150da18494afcedffe73485f8e4621
jareehd <dheeraj945016@gmail.com>
2021-06-24 11:23:24 +0530

    added header

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import Header from './Components/Header';
+import {HashRouter, Route ,Switch} from 'react-router-dom'
+import Header from './Components/Header.js'
+import Create from './Components/Create.js'
+import View from './Components/View.js'
+import Edit from './Components/Edit.js'
 
 function App() {
   return (
     <div className="App">
-        Interview Scheduler
-        
+      <HashRouter>
+        <Header/>
+          <Switch>
+             <Route exact path='/' component = {Create} />
+             <Route path='/view' component = {View} />
+             <Route path='/edit' component = {Edit} />             
+          </Switch>
+      </HashRouter>
     </div>
   );
 }
---
+++ client/src/Components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar , Toolbar ,Typography ,Button } from '@material-ui/core'
+import { Link  } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+    title: {
+      flexGrow: 1,
+      padding:10    
+    },
+  }));
+
+const Header = () =>{
+    const classes = useStyles();
+    return (
+    <AppBar position="static">
+    <Toolbar variant="dense">
+    <Typography align ="left" variant="h4" color="inherit" className={classes.title}>
+      Interview Bytes
+    </Typography>
+    
+    <div > 
+     
+     <Button color="inherit"> 
+      <Link to="/" style={{ textDecoration: "none", color:'white', width: "100%" }} >
+                   Create Interviews
+       </Link> 
+     </Button>
+     <Button color="inherit"> 
+      <Link to="/view" style={{ textDecoration: "none", color:'white', width: "100%" }} >
+                   View Interviews
+       </Link>
+     </Button>
+     <Button color="inherit"> 
+       <Link to="/edit" style={{ textDecoration: "none", color:'white', width: "100%" }} >
+                   Edit Interviews
+       </Link>
+       </Button>
+    </div>
+  </Toolbar>
+</AppBar>
+    )
+}
+
+export default Header;
